d7a97d9eca555541f06568bfd0c84352a209468a
Javier Antón Moral <javier.anton@juanxxiii.net>
2019-05-31 19:24:05 +0200

    PONER BOTON EN AMARILLO

--- WINCONGIT/WINCONGIT/Form1.Designer.cs
+++ WINCONGIT/WINCONGIT/Form1.Designer.cs
@@ -34,12 +34,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(161, 57);
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button1.Location = new System.Drawing.Point(63, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(183, 102);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
@@ -61,6 +62,7 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- WINCONGIT/WINCONGIT/Form1.cs
+++ WINCONGIT/WINCONGIT/Form1.cs
@@ -21,5 +21,10 @@ namespace WINCONGIT
         {
             MessageBox.Show("ESTAMOS EN CONTROL DE VERSIONES");
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
