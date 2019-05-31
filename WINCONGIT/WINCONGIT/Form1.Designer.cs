d2cbee21171553cd867f0be6017518f8007d8f65
Javier Antón Moral <javier.anton@juanxxiii.net>
2019-05-31 19:28:33 +0200

    OTRA VEZ BOTON EN AMARILLO

--- WINCONGIT/WINCONGIT/Form1.Designer.cs
+++ WINCONGIT/WINCONGIT/Form1.Designer.cs
@@ -35,9 +35,9 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.button1.Location = new System.Drawing.Point(63, 12);
+            this.button1.Location = new System.Drawing.Point(51, 74);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(183, 102);
+            this.button1.Size = new System.Drawing.Size(205, 138);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = false;
